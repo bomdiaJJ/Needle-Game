4a6b7b991a380008057d393f6f9bf8818d3c8e6d
JJ <joaoart42@gmail.com>
2022-05-16 15:06:15 -0300

    feat: add FloatVariable modular data

---
+++ Assets/Scripts/FloatVariable.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "FloatVariable", menuName = "Modular Data/Float Variable")]
+public class FloatVariable : ScriptableObject {
+	[SerializeField] private float _value;
+
+	public event Action<float> OnValueChanged;
+
+	public float Value {
+		get {
+			return _value;
+		}
+		set {
+			_value = value;
+			OnValueChanged?.Invoke(_value);
+		}
+	}
+}
