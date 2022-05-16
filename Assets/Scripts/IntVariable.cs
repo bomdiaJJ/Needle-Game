154c0758963b28cf32da35642d955db0830a7998
JJ <joaoart42@gmail.com>
2022-05-16 15:07:32 -0300

    feat: add IntVariable modular data

---
+++ Assets/Scripts/IntVariable.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IntVariable", menuName = "Modular Data/Int Variable")]
+public class IntVariable : ScriptableObject {
+	[SerializeField] private int _value;
+
+	public event Action<int> OnValueChanged;
+
+	public int Value {
+		get {
+			return _value;
+		}
+		set {
+			_value = value;
+			OnValueChanged?.Invoke(_value);
+		}
+	}
+}
