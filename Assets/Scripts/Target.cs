c8bafd68c87e22f65d0523abdeb300cdc44f9323
JJ <joaoart42@gmail.com>
2022-05-17 05:02:04 -0300

    feat: support for different UnityEvents by hit tag
    Target now has a different UnityEvent for each tag he collides with

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -1,12 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using Sirenix.OdinInspector;
 
-public class Target : MonoBehaviour {
-	[SerializeField] private UnityEvent OnHitByPlayer;
-	
+public class Target : SerializedMonoBehaviour {
+	[SerializeField] private Dictionary<string, UnityEvent> OnHitByTag = new Dictionary<string, UnityEvent>();
+
 	private void OnTriggerEnter2D(Collider2D other) {
-		if (other.gameObject.tag == "Player") {
-			OnHitByPlayer?.Invoke();
+		if (OnHitByTag.ContainsKey(other.tag)) {
+			OnHitByTag[other.tag]?.Invoke();
 		}
 	}
 
